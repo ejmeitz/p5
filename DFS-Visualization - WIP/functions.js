1dea4422fbc9b293cab81160331f2baaa4d34a9e
Ethan Meitz <ejmeitz1@gmail.com>
2020-08-11 20:44:32 -0500

    dfs algorithm works

--- DFS-Visualization - WIP/functions.js
+++ DFS-Visualization - WIP/functions.js
@@ -13,3 +13,73 @@ function getLegalY(diameter) {
     }
     return y;
 }
+
+function dfs(nodes, currentNode, discoveryOrder){
+  currentNode.visited = true;
+  visitedNodes.push(currentNode.value);
+  discoveryOrder.push(currentNode.value);
+    currentNode.neighbors.forEach((w) => {
+      if(!visitedNodes.includes(w)){
+        dfs(nodes, nodes[w], discoveryOrder); //w is just the .value of the node so we can access the actual node by accessing that index
+      }
+    });
+}
+
+function createRandomConnectivity(nodes, maxConnections){
+  for (let i = 0; i < nodes.length;i++){
+      let connections = Math.floor(maxConnections * Math.random()) + 1; //plus one cause I don't want zero connections even tho DFS would still work
+      for(let j = 0; j < connections ; j++){
+        let connectedNodeValue = i; // start with the condition that the node tries to connect to itself so while loop triggers
+        while(connectedNodeValue == nodes[i].value || nodes[i].neighbors.includes(connectedNodeValue)){ // if the node tries to connect to itself or a node its already connected to try again
+            connectedNodeValue = Math.floor(numNodes*Math.random());
+        }
+        nodes[i].neighbors.push(connectedNodeValue);
+        if(!nodes[connectedNodeValue].neighbors.includes(i)){
+          nodes[connectedNodeValue].neighbors.push(i);
+        }
+
+        //draw lines
+        let x1 = (nodes[i].x );
+        let y1 = (nodes[i].y );
+        let x2 = (nodes[connectedNodeValue].x);
+        let y2 = (nodes[connectedNodeValue].y);
+        strokeWeight(3);
+        line(x1,y1,x2,y2);
+      }
+  }
+  console.log(nodes);
+}
+
+function createRandomGrid(){
+    let nodes = [];
+
+    for(let i = 0; i < numNodes; i++){
+            let x1 ;
+            let y1;
+
+            let legal = false;
+            while(!legal){
+              x1 = getLegalX(d);
+              y1 = getLegalY(d);
+
+
+              if(i >= 1){ //check if node overlaps with an already existing node  -- maybe if its even close at all try again
+                for(let j = 0; j < i ; j++){
+                    let dist = Math.sqrt(Math.pow((nodes[j].x - x1),2) + Math.pow((nodes[j].y - y1),2));
+                    if(dist < 8*d){
+                        legal = false;
+                        break; //try again
+                      } else {
+                        legal = true;
+                      }
+                  }
+                } else {
+                  break;
+                }
+            }
+
+            let temp = new Node(x1, y1, i);
+            nodes.push(temp);
+    }
+    return nodes;
+}
--- DFS-Visualization - WIP/sketch.js
+++ DFS-Visualization - WIP/sketch.js
@@ -1,5 +1,6 @@
-let numNodes = 10;  // must be less than rows*cols
+let numNodes = 7;  // must be less than rows*cols
 let d = 50;
+let visitedNodes = [];
 
 
 class Node {
@@ -8,76 +9,16 @@ class Node {
    this.x = x; //grid x val (0-4)
    this.y = y; //grid y val (0-4)
    this.value = value;
+   this.visited = false;
  }
 }
-
-function createRandomGrid(){
-    let nodes = [];
-
-    for(let i = 0; i < numNodes; i++){
-            let x1 ;
-            let y1;
-
-            let legal = false;
-            while(!legal){
-              x1 = getLegalX(d);
-              y1 = getLegalY(d);
-
-
-              if(i >= 1){ //check if node overlaps with an already existing node  -- maybe if its even close at all try again
-                for(let j = 0; j < i ; j++){
-                    let dist = Math.sqrt(Math.pow((nodes[j].x - x1),2) + Math.pow((nodes[j].y - y1),2));
-                    if(dist < 4*d){
-                        legal = false;
-                        break; //try again
-                      } else {
-                        legal = true;
-                      }
-                  }
-                } else {
-                  break;
-                }
-            }
-
-            let temp = new Node(x1, y1, i);
-            nodes.push(temp);
-    }
-    return nodes;
-}
-
-function createRandomConnectivity(nodes, maxConnections){
-  for (let i = 0; i < nodes.length;i++){
-      let connections = Math.floor(maxConnections * Math.random()) + 1; //plus one cause I don't want zero connections even tho DFS would still work
-      for(let j = 0; j < connections ; j++){
-        let connectedNodeValue = i; // start with the condition that the node tries to connect to itself so while loop triggers
-        while(connectedNodeValue == nodes[i].value || nodes[i].neighbors.includes(connectedNodeValue)){ // if the node tries to connect to itself or a node its already connected to try again
-            connectedNodeValue = Math.floor(numNodes*Math.random());
-        }
-        nodes[i].neighbors.push(connectedNodeValue);
-        if(!nodes[connectedNodeValue].neighbors.includes(i)){
-          nodes[connectedNodeValue].neighbors.push(i);
-        }
-
-        //draw lines
-        let x1 = (nodes[i].x );
-        let y1 = (nodes[i].y );
-        let x2 = (nodes[connectedNodeValue].x);
-        let y2 = (nodes[connectedNodeValue].y);
-        strokeWeight(3);
-        line(x1,y1,x2,y2);
-      }
-  }
-  console.log(nodes);
-}
-
-//imagine the canvas as a 5x5 grid of nodes some of which may be active...add connectivity (neighbors) then apply alg
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
   let nodes = createRandomGrid();
-  let count = 0;
 
-  createRandomConnectivity(nodes,2); //this is first so the lines stay behind the circles;
+//the second param can be a decimal 1.25 means on average given infinity nodes they will have 1.25 connections originating from them or 2.5 total
+  createRandomConnectivity(nodes,1.5); //this is first so the lines stay behind the circles;
 
   //draw the circles;
   for(let i = 0; i < nodes.length; i++){
@@ -87,10 +28,13 @@ function setup() {
       circle(x , y , d); //maybe make elipses if needs to be responsive and use boxHeight
       textSize(30);
       textAlign(CENTER, CENTER);
-      text(count, x, y)
-      count += 1;
+      text(i, x, y)
   }
 
+  // run recursive dfs to get discovery order
+  let discoveryOrder = []; //right now they're stored as values not the actual node object
+  dfs(nodes, nodes[0], discoveryOrder);
+  console.log("Discovery Order:" + discoveryOrder);
 }
 
 function draw() {
